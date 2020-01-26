1f875c6d3636fa18fb8f1fcccdd52ae86257ae8f
lsqrouse <rouse8@purdue.edu>
2020-01-25 20:07:25 -0500

    fixed random errors and fixed url search functionality

--- Chrome Extension Testing/userPage/loadUser.js
+++ Chrome Extension Testing/userPage/loadUser.js
@@ -1,43 +1,37 @@
+document.getElementById("mainBody").addEventListener("load", GenerateTable());
+    
 function GenerateTable() {
         //Build an array containing Customer records.
-        var sites = new Array();
+        console.log("here");
         var jsonArray = new Array();
-        //testing json array
-        var json1 = '{"website": "BBC", "Title": "Dan doing bad things", "URL": "www.com", "Percent": 95.7}';
-			for(int i = 0; i < 7; i++) {
-				var obj = JSON.parse(json1);
+        jsonArray.push(["Website", "Title", "URL", "Confidence Percentage"])
+        var json1 = '{"website": "BBC", "title": "Dan doing bad things", "url": "www.com", "percent": 95.7}';
+        var obj = JSON.parse(json1);
 				jsonArray.push([obj.website, obj.title, obj.url, obj.percent]);
-			}
+				jsonArray.push([obj.website, obj.title, obj.url, obj.percent]);
+				jsonArray.push([obj.website, obj.title, obj.url, obj.percent]);
 
-        sites.push(["Website", "Title", "Url", "Percent"]);
-        sites.push(["Buzzfeed", "Why Dan should rule the world", "buzzfeed.com", 98.5]);
-        for(var i = 0 i < jsonArray.length; i++) {
-        		var obj = JSON.parse(jsonArray[i]);
-        		sites.push([obj.website, obj.title, obj.url, obj.percent]);
-        }
-
- 
         //Create a HTML Table element.
         var table = document.createElement("TABLE");
         table.border = "1";
  
         //Get the count of columns.
-        var columnCount = sites[0].length;
+        var columnCount = jsonArray[0].length;
  
         //Add the header row.
         var row = table.insertRow(-1);
         for (var i = 0; i < columnCount; i++) {
             var headerCell = document.createElement("TH");
-            headerCell.innerHTML = sites[0][i];
+            headerCell.innerHTML = jsonArray[0][i];
             row.appendChild(headerCell);
         }
  
         //Add the data rows.
-        for (var i = 1; i < sites.length; i++) {
+        for (var i = 1; i < jsonArray.length; i++) {
             row = table.insertRow(-1);
             for (var j = 0; j < columnCount; j++) {
                 var cell = row.insertCell(-1);
-                cell.innerHTML = sites[i][j];
+                cell.innerHTML = jsonArray[i][j];
             }
         }
  
